85d6a5e80e1e456406d06884ecd52cdf0102c6ee
Yushmanov Anton <antoxa327@gmail.com>
2022-04-20 17:04:17 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+void RemoveDups(char* str) {
+	size_t j = 1;
+	size_t len = strlen(str);
+	char prev = str[0];
+
+	for (size_t i = 1; i < len; i++){
+		if (str[i] != prev){
+			prev = str[i];
+			str[j] = prev;
+			j++;
+		}
+	}
+	str[j] = '\0';
+}
+
+int main() {
+	char data[] = "AAA BBB AAA";
+
+	RemoveDups(data);
+	printf("%s\n", data); // "A B A"
+
+	return 0;
+}
